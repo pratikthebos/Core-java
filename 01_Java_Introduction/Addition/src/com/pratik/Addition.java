435df682b7f21914a8e13dea6e0978b92e5ed631
pratik kambale <pratikkambale@cdacmumbai.in>
2026-01-02 16:14:09 +0530

    Addition program

---
+++ 01_Java_Introduction/Addition/src/com/pratik/Addition.java
@@ -0,0 +1,25 @@
+package com.pratik;
+import java.util.Scanner;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int a = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int b = sc.nextInt();
+
+        int sum = a + b;
+
+        System.out.println("Addition = " + sum);
+
+        sc.close();
+
+	}
+
+}
---
+++ 01_Java_Introduction/BasicJava/src/com/pratik/Test.java
@@ -0,0 +1,11 @@
+package com.pratik;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello my name is pratik ....");
+
+	}
+
+}
