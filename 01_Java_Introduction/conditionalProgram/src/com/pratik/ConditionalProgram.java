7955890d3e10f89df96d99178701899432250ec5
Pratik Kambale <pratikvkambale53@gmail.com>
2026-01-09 18:49:02 +0530

    Conditional program

---
+++ 01_Java_Introduction/conditionalProgram/src/com/pratik/ConditionalProgram.java
@@ -0,0 +1,16 @@
+package com.pratik;
+
+public class ConditionalProgram {
+
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 20;
+
+        if (a > b) {
+            System.out.println("A is greater");
+        } else {
+            System.out.println("B is greater");
+        }
+    }
+}
