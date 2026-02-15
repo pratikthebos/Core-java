ba235d49362a6af9214a9baa11370b478e8c38a3
Pratik Kambale <pratikvkambale53@gmail.com>
2026-02-15 22:08:57 +0530

    factorial program

---
+++ 02_Java_mid level code/FactorialProgram/src/com/pratik/FactorialProgram.java
@@ -0,0 +1,16 @@
+package com.pratik;
+
+public class FactorialProgram {
+
+    public static void main(String[] args) {
+
+        int num = 5;
+        long factorial = 1;
+
+        for (int i = 1; i <= num; i++) {
+            factorial = factorial * i;
+        }
+
+        System.out.println("Factorial of " + num + " is: " + factorial);
+    }
+}
