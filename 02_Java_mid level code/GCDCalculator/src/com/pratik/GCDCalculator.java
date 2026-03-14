ef07b0197accd1947aa1d4af25e3bdf5fa35f81d
Pratik Kambale <pratikvkambale53@gmail.com>
2026-03-14 18:28:24 +0530

    gc calculator code

---
+++ 02_Java_mid level code/GCDCalculator/src/com/pratik/GCDCalculator.java
@@ -0,0 +1,34 @@
+package com.pratik;
+
+import java.util.Scanner;
+
+public class GCDCalculator {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int a = scanner.nextInt();
+
+        System.out.print("Enter second number: ");
+        int b = scanner.nextInt();
+
+        int gcd = findGCD(a, b);
+
+        System.out.println("GCD of " + a + " and " + b + " is: " + gcd);
+
+        scanner.close();
+    }
+
+    public static int findGCD(int a, int b) {
+
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+
+        return a;
+    }
+}
