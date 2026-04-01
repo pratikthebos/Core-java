241b70e8421a7a8756dafa373b781fd95598a7ed
Pratik Kambale <pratikvkambale53@gmail.com>
2026-04-01 20:22:27 +0530

    move zero code program

---
+++ 03_java/MoveZeroes/src/com/pratik/MoveZeroes.java
@@ -0,0 +1,33 @@
+package com.pratik;
+
+public class MoveZeroes {
+
+    public static void moveZeroes(int[] nums) {
+
+        int index = 0;
+
+        // Move non-zero elements forward
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[index++] = nums[i];
+            }
+        }
+
+        // Fill remaining with zeroes
+        while (index < nums.length) {
+            nums[index++] = 0;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {0, 1, 0, 3, 12};
+
+        moveZeroes(nums);
+
+        System.out.print("Result: ");
+        for (int num : nums) {
+            System.out.print(num + " ");
+        }
+    }
+}
