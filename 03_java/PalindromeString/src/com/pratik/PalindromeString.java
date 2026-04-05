c0a3f3d2874429866fad82c368ef08707bd9091d
Pratik Kambale <pratikvkambale53@gmail.com>
2026-04-05 18:56:25 +0530

    palindrome string code program

---
+++ 03_java/PalindromeString/src/com/pratik/PalindromeString.java
@@ -0,0 +1,31 @@
+package com.pratik;
+
+public class PalindromeString {
+
+    public static boolean isPalindrome(String str) {
+
+        int left = 0;
+        int right = str.length() - 1;
+
+        while (left < right) {
+
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        String input = "madam";
+
+        boolean result = isPalindrome(input);
+
+        System.out.println("Is Palindrome: " + result);
+    }
+}
