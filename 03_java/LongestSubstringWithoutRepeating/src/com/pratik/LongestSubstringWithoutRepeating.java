3a78bc0caf307522eb515ecbd1bfb142b8f4277d
Pratik Kambale <pratikvkambale53@gmail.com>
2026-04-20 18:19:57 +0530

    longest sub string without repeating

---
+++ 03_java/LongestSubstringWithoutRepeating/src/com/pratik/LongestSubstringWithoutRepeating.java
@@ -0,0 +1,36 @@
+package com.pratik;
+
+import java.util.*;
+
+public class LongestSubstringWithoutRepeating {
+
+    public static int lengthOfLongestSubstring(String s) {
+
+        Set<Character> set = new HashSet<>();
+
+        int left = 0;
+        int maxLength = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+
+            while (set.contains(s.charAt(right))) {
+                set.remove(s.charAt(left));
+                left++;
+            }
+
+            set.add(s.charAt(right));
+            maxLength = Math.max(maxLength, right - left + 1);
+        }
+
+        return maxLength;
+    }
+
+    public static void main(String[] args) {
+
+        String input = "abcabcbb";
+
+        int result = lengthOfLongestSubstring(input);
+
+        System.out.println("Longest substring length: " + result);
+    }
+}
