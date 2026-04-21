cbcef63bd984f7189c029e99cc82914434bc9f73
Pratik Kambale <pratikvkambale53@gmail.com>
2026-04-21 17:23:28 +0530

    word break

---
+++ 03_java/WordBreak/src/com/pratik/WordBreak.java
@@ -0,0 +1,37 @@
+package com.pratik;
+
+import java.util.*;
+
+public class WordBreak {
+
+    public static boolean wordBreak(String s, List<String> wordDict) {
+
+        Set<String> set = new HashSet<>(wordDict);
+        boolean[] dp = new boolean[s.length() + 1];
+
+        dp[0] = true; // empty string
+
+        for (int i = 1; i <= s.length(); i++) {
+
+            for (int j = 0; j < i; j++) {
+
+                if (dp[j] && set.contains(s.substring(j, i))) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+
+        return dp[s.length()];
+    }
+
+    public static void main(String[] args) {
+
+        String s = "leetcode";
+        List<String> wordDict = Arrays.asList("leet", "code");
+
+        boolean result = wordBreak(s, wordDict);
+
+        System.out.println("Can break: " + result);
+    }
+}
