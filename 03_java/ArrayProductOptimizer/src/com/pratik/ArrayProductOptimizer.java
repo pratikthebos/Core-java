93e258e01b4886e3581d567bdf381b6e1dbdaf3a
Pratik Kambale <pratikvkambale53@gmail.com>
2026-05-12 17:11:55 +0530

    array procution optimize

---
+++ 03_java/ArrayProductOptimizer/src/com/pratik/ArrayProductOptimizer.java
@@ -0,0 +1,41 @@
+package com.pratik;
+
+import java.util.*;
+
+public class ArrayProductOptimizer {
+
+    public static int[] productExceptSelf(int[] nums) {
+
+        int n = nums.length;
+        int[] result = new int[n];
+
+        // Left product
+        result[0] = 1;
+        for (int i = 1; i < n; i++) {
+            result[i] = result[i - 1] * nums[i - 1];
+        }
+
+        // Right product
+        int rightProduct = 1;
+
+        for (int i = n - 1; i >= 0; i--) {
+            result[i] *= rightProduct;
+            rightProduct *= nums[i];
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {1, 2, 3, 4};
+
+        int[] result = productExceptSelf(nums);
+
+        System.out.print("Product Except Self: ");
+
+        for (int num : result) {
+            System.out.print(num + " ");
+        }
+    }
+}
