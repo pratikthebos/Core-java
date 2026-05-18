49779ebc313976005111d20e6f3d3d30bcecff6b
Pratik Kambale <pratikvkambale53@gmail.com>
2026-05-18 18:54:32 +0530

    temperature fore cast analyzer

---
+++ 03_java/TemperatureForecastAnalyzer/src/com/pratik/TemperatureForecastAnalyzer.java
@@ -0,0 +1,41 @@
+package com.pratik;
+
+import java.util.*;
+
+public class TemperatureForecastAnalyzer {
+
+    public static int[] dailyTemperatures(int[] temperatures) {
+
+        int n = temperatures.length;
+        int[] result = new int[n];
+
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < n; i++) {
+
+            while (!stack.isEmpty() &&
+                    temperatures[i] > temperatures[stack.peek()]) {
+
+                int index = stack.pop();
+                result[index] = i - index;
+            }
+
+            stack.push(i);
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+        int[] temperatures = {73,74,75,71,69,72,76,73};
+
+        int[] result = dailyTemperatures(temperatures);
+
+        System.out.println("Days Until Warmer Temperature:");
+
+        for (int day : result) {
+            System.out.print(day + " ");
+        }
+    }
+}
---
+++ 03_java/TemperatureForecastAnalyzer/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module TemperatureForecastAnalyzer {
+}
