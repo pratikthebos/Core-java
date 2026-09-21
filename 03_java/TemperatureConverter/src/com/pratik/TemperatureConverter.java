6491191c5438e7a5bca75d9f1a0c5133f6b46bd7
Pratik Kambale <pratikvkambale53@gmail.com>
2026-09-21 17:27:26 +0530

    temperature converter java code

---
+++ 03_java/TemperatureConverter/src/com/pratik/TemperatureConverter.java
@@ -0,0 +1,20 @@
+package com.pratik;
+
+import java.util.Scanner;
+
+public class TemperatureConverter {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter temperature in Celsius: ");
+        double celsius = sc.nextDouble();
+
+        double fahrenheit = (celsius * 9 / 5) + 32;
+
+        System.out.println("Fahrenheit: " + fahrenheit);
+
+        sc.close();
+    }
+}
